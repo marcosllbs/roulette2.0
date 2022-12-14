cda043006dba200a7a271f3521ebbd48a0e0912b
Marcos Lucas <42158901+marcosllbs@users.noreply.github.com>
2022-12-14 01:58:13 -0300

    prefab instantiate

---
+++ Assets/Scripts/ButtonBetScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ButtonBetScript : MonoBehaviour
+{
+    public TMP_Text betNumber;
+    public Button betButton;
+}
--- Assets/Scripts/RouletteManager.cs
+++ Assets/Scripts/RouletteManager.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 
 public class RouletteManager : MonoBehaviour
 {
     [SerializeField] GameObject rouletteGameObject;
+    [SerializeField] GameObject buttonPrefab;
+    [SerializeField] Transform transformToInstantiate;
     bool isSpining = false;
     public int angles = 0;
+    public int points = 0;
     private float rotationSpeed = 0;
+
+    public List<ButtonBetScript> buttonBetScriptsList = new List<ButtonBetScript>();
 
 
     // Start is called before the first frame update
     void Start()
 
     {
-
+        SetButtonsInstantiate();
     }
 
     // Update is called once per frame
@@ -39,28 +46,40 @@ public class RouletteManager : MonoBehaviour
     }
     public void SpinRoulette()
     {
-        //rouletteGameObject.transform.DORotate(new Vector3(0, 0, Mathf.PingPong(0,360)), rotationDuration, RotateMode.FastBeyond360);
         RotationSpeed();
-
         isSpining = true;
-
         rouletteGameObject.transform.Rotate(new Vector3(0, 0, 1), rotationSpeed);
-
-        //rouletteGameObject.transform.Rotate()
-
-        Invoke("SpinRouletteStop", Random.Range(0.8f, 1.8f));
 
     }
     public void SpinRouletteStop()
     {
-        angles = (int)(rouletteGameObject.GetComponent<Transform>().localEulerAngles.z)/45;
-        isSpining = false;
-        Debug.Log($"O angulo e: {angles}");
         rouletteGameObject.transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * 0.1f);
+        isSpining = false;
+        angles = (int)(rouletteGameObject.GetComponent<Transform>().localEulerAngles.z) / 45;
+        Debug.Log($"O angulo e: {angles}");
     }
 
-    public void SpinRoulletStopCallBack()
+    public void SetButtonsInstantiate()
     {
-        
+        GameObject buttonsInstatiated;
+        ButtonBetScript buttonsInstantiatedScript;
+        for (int i = 0; i < 8; i++)
+        {
+            buttonsInstatiated = Instantiate(buttonPrefab, transformToInstantiate);
+            buttonsInstantiatedScript = buttonsInstatiated.GetComponent<ButtonBetScript>();
+            buttonBetScriptsList.Add(buttonsInstantiatedScript);
+            InitializeButtons(buttonBetScriptsList[i].betButton, buttonBetScriptsList[i].betNumber, i);
+        }
+    }
+
+    public void InitializeButtons(Button button, TMP_Text text, int i)
+    {
+        button.onClick.AddListener(() => SpinRoulette()); Invoke("SpinRouletteStop", Random.Range(0.5f, 1f));
+        text.SetText(i.ToString());
+    }
+
+    public void SpinRouletteStopCallBack()
+    {
+        Invoke("SpinRouletteStop", Random.Range(0.5f, 1f));
     }
 }
