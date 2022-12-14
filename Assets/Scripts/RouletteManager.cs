fdf3de796ccb97961238994da23356e29c19fc53
Marcos Lucas <42158901+marcosllbs@users.noreply.github.com>
2022-12-14 00:04:37 -0300

    Fixes

--- Assets/Scripts/RouletteManager.cs
+++ Assets/Scripts/RouletteManager.cs
@@ -8,6 +8,8 @@ public class RouletteManager : MonoBehaviour
 {
     [SerializeField] GameObject rouletteGameObject;
     bool isSpining = false;
+    public int angles = 0;
+    private float rotationSpeed = 0;
 
 
     // Start is called before the first frame update
@@ -30,20 +32,35 @@ public class RouletteManager : MonoBehaviour
 
 
     }
+
+    public void RotationSpeed()
+    {
+        rotationSpeed = Random.Range(100f, 300f) * Time.deltaTime;
+    }
     public void SpinRoulette()
     {
         //rouletteGameObject.transform.DORotate(new Vector3(0, 0, Mathf.PingPong(0,360)), rotationDuration, RotateMode.FastBeyond360);
-        
+        RotationSpeed();
+
         isSpining = true;
 
-        rouletteGameObject.transform.Rotate(new Vector3(0, 0, 1),Random.Range(1f,45f));
-        
+        rouletteGameObject.transform.Rotate(new Vector3(0, 0, 1), rotationSpeed);
+
+        //rouletteGameObject.transform.Rotate()
+
+        Invoke("SpinRouletteStop", Random.Range(0.8f, 1.8f));
 
     }
     public void SpinRouletteStop()
     {
-        float angles = rouletteGameObject.GetComponent<Transform>().localEulerAngles.z;
+        angles = (int)(rouletteGameObject.GetComponent<Transform>().localEulerAngles.z)/45;
         isSpining = false;
         Debug.Log($"O angulo e: {angles}");
+        rouletteGameObject.transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * 0.1f);
+    }
+
+    public void SpinRoulletStopCallBack()
+    {
+        
     }
 }
